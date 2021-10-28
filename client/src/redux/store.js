4bb0bd8dc939296408b7378a8643967634a997a0
SitaelJs <klimanovfresh@gmail.com>
2021-10-28 16:39:07 +0300

    init project

---
+++ client/src/redux/store.js
@@ -0,0 +1,20 @@
+import { applyMiddleware, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
+import initState from './initState'
+import rootReducer from './reducers/rootReducer'
+import rootSaga from "./sagas/rootSaga";
+
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(
+    rootReducer,
+    initState,
+    applyMiddleware(thunk, sagaMiddleware),
+)
+
+sagaMiddleware.run(rootSaga)
+
+
+
+export default store
